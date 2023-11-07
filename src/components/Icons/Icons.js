0d5325712f7a952309bfd849764d8fbd8403fcd1
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-07 21:30:48 +0530

    file for icons export

---
+++ src/components/Icons/Icons.js
@@ -0,0 +1,9 @@
+import { IoArrowBackOutline } from "react-icons/io5";
+import { BsFillMicFill } from "react-icons/bs";
+import { GoSearch } from "react-icons/go";
+
+export {IoArrowBackOutline,GoSearch,BsFillMicFill}
+
+
+
+
