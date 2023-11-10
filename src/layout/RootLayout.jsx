9bb9cf0328ea2009659b5be746960a264f9d3641
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-10 21:23:49 +0530

    "unused declaration" removal

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import {
-  Categories,
   FixedSideBar,
   Navbar,
   SideBarContainer,
