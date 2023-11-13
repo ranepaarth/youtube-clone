79609c866985f6ee220a8c2715c3a2ebab13d4b3
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-13 12:17:32 +0530

    a component for side nav item

---
+++ src/components/SideBar/SideBarNavItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SideBarNavItem = ({icon,text,className,action}) => {
+  return (
+    <span className={'flex items-center gap-5 px-3 py-2 hover:bg-[#161616] rounded-md [transition-timing-function:ease] [transition-duration:300ms] hover:bg-white/[0.15] ' + className} onClick={action} onKeyDown={action}>
+      <p className=''>{icon}</p>
+      <p className='capitalize'>{text}</p>
+    </span>
+  )
+}
+
+export default SideBarNavItem
