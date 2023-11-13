533a6861fe3a40775e5750ded9456c57fb83e96f
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-13 12:15:46 +0530

    changed padding of logo span

--- src/components/NavBar/AppLogo.jsx
+++ src/components/NavBar/AppLogo.jsx
@@ -6,7 +6,7 @@ const AppLogo = () => {
   return (
     <Link to="/" className="flex items-center p-2 gap-2 bg-[#0a0a0a]">
       <Hamburger />
-      <span className="flex items-center gap-1">
+      <span className="flex items-center gap-1 lg:pl-4">
         <img src={Logo} alt="TechieTube" />
         <span className="text-xl font-bold tracking-[-0.08rem] md:block md:visible">
           TechieTube
