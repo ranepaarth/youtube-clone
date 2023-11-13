4ad2c9290530313e13149fa2f6f013931c7de4f4
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-13 12:15:10 +0530

    changed styles for main Outlet and fixedSidebar

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -11,22 +11,28 @@ import LoadingBar from "react-top-loading-bar";
 
 const RootLayout = () => {
   const { open, progress, setProgress } = useAppContext();
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
   console.log(pathname);
   return (
     <>
-      <div className="fixed top-0 left-0 right-0 bg-[#0a0a0a] select-none">
+      <div className="fixed top-0 left-0 right-0 bg-[#0a0a0a] select-none z-10">
         <Navbar setProgress={setProgress} />
-        <FixedSideBar />
+        <div className="lg:hidden block">
+          <FixedSideBar />
+        </div>
       </div>
       <LoadingBar color="red" height="2px" progress={progress} />
       <div>
         <SideBarContainer />
       </div>
       {/* <main className="h-[200vh] mt-32 mx-3 mb-3 md:ml-28 px-6  bg-neutral-800 ">{<Outlet setProgress={setProgress} />}</main> */}
-      {pathname === '/' || pathname === '/video'?<main className="h-[200vh] mt-12 mx-4 my-4 scroll-smooth select-none">
-        {<Outlet setProgress={setProgress} />}
-      </main> : <main className="select-none">{<Outlet/>}</main>}
+      {pathname === "/" || pathname === "/video" ? (
+        <main className="mx-2 scroll-smooth overflow-y-hidden">
+          {<Outlet setProgress={setProgress} />}
+        </main>
+      ) : (
+        <main className="">{<Outlet />}</main>
+      )}
     </>
   );
 };
